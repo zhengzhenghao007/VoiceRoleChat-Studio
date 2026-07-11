514b61d5884927a2f5cfd1b2b35e0a217e2a94fa
Sihan Zheng <szhe0714@uni.sydney.edu.au>
2026-07-11 21:44:03 +1000

    Add initial local web interface

---
+++ frontend/app.js
@@ -0,0 +1,9 @@
+const createCharacterButton = document.getElementById(
+    "createCharacterButton"
+);
+
+createCharacterButton.addEventListener("click", () => {
+    alert(
+        "The character creation page will be added in the next development step."
+    );
+});
